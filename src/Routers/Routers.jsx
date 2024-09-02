a4c3b53b79de4cffc62ce6999abc9ab64ab62eb8
unknown <mehedytouhidurzaman@gmail.com>
2024-09-02 12:44:13 +0600

    router completed

---
+++ src/Routers/Routers.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Root from '../Layout/Root';
+import Home from '../Pages/Home';
+import Login from '../Pages/Login';
+import Register from '../Pages/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        },
+        {
+            path:"/login",
+            element: <Login></Login>
+        },
+        {
+            path: "/register",
+            element: <Register></Register>
+        }
+],
+  },
+]);
+
+export default router
